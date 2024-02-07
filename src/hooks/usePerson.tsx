6391baa8422224f24ba3f01052f518c27feb9601
Ben Winchester <benjamin.winchester@madwire.com>
2024-02-07 15:08:32 -0500

    invoke task in usePerson

--- src/hooks/usePerson.tsx
+++ src/hooks/usePerson.tsx
@@ -1,7 +1,6 @@
-import { map } from "fp-ts/Task";
-import { fold } from "fp-ts/Either";
+import { fold } from "fp-ts/TaskEither";
+import * as Task from "fp-ts/Task";
 import { pipe } from "fp-ts/lib/function";
-import { IO } from "fp-ts/IO";
 import { useCallback, useEffect, useState } from "react";
 import getPerson, { SwapiPerson } from "../fetchers/getPerson";
 import { peek } from "../utils/peek";
@@ -11,29 +10,23 @@ const usePerson = (personId: number) => {
   const [error, setError] = useState<string>();
   const [person, setPerson] = useState<SwapiPerson>();
 
-  const toggleLoading: IO<void> = () => {
-    setIsLoading((prev) => !prev);
-  };
-
   const fetchPersonById = useCallback(
     async (id: number) =>
       pipe(
         id,
         getPerson,
         peek("inside hook"),
-        map(
-          fold(
-            (e) => setError(e.message),
-            (person) => setPerson(person),
-          ),
+        fold(
+          (e) => Task.of(setError(e.message)),
+          (person: SwapiPerson) => Task.of(setPerson(person)),
         ),
-      ),
+      )(),
     [],
   );
 
   useEffect(() => {
-    toggleLoading();
-    fetchPersonById(personId).finally(toggleLoading);
+    setIsLoading(true);
+    fetchPersonById(personId).finally(() => setIsLoading(false));
   }, [fetchPersonById, personId]);
 
   return {
